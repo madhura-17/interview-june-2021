df1b99595a7c67dd7591ea39536cd7853336988b
Madhuri Sanjay Jadhav <60167309+madhura-17@users.noreply.github.com>
2021-06-22 12:48:18 +0530

    completed

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -1,9 +1,37 @@
 package exam;
 
+class MyException extends Exception{
+   String a;
+   MyException(String b) {
+     a=b;
+   }
+}
+
 public class ConvertToBinary {
 
-  public String toBinary(final int n) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return "";
-  }
+  public String toBinary(final int n1) {
+try {
+          if (n1 < 0){
+          throw new MyException(" It doesn't still supports to convert the negative number into binary");
+      }
+      }catch(MyException e){
+    System.out.println(e.a) ;
+ }
+
+    String res = "";
+    int n = n1;
+    for(int i=0;n>0;i++)    
+   {    
+     res= res + n%2;    
+     n=n/2;    
+   } 
+   
+   String reverse = "";
+   
+   for (int i = res.length()-1 ; i >= 0 ; i--){
+       reverse= reverse + res.charAt(i);
+   }
+   
+   return reverse;
+ }
 }
